009c494ae2f4bf3c84763a43f1606fc13310f534
Yusuf Yudhistira <yudhis96@yahoo.com>
2023-05-01 19:43:32 +0700

    Membuat CRUD Agama

---
+++ backend/models/Agama.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "agama".
+ *
+ * @property int $id
+ * @property string $nama
+ */
+class Agama extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'agama';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['nama'], 'required'],
+            [['nama'], 'string', 'max' => 45],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'nama' => 'Nama',
+        ];
+    }
+}
