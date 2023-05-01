6c16f65cc9070d15ba6409177872565b2358b745
Yusuf Yudhistira <yudhis96@yahoo.com>
2023-05-01 19:51:13 +0700

    Menambah dan mengubah link sidebar

--- backend/views/layouts/sidebar.php
+++ backend/views/layouts/sidebar.php
@@ -45,14 +45,25 @@
                         'header' => true,
                     ],
                     [
+                        'label' => 'Data LookUp',
+                        'icon' => 'database',
+                        'items' => [
+                            ['label' => 'Agama',  'icon' => 'building', 'url' => ['/agama']],
+                            ['label' => 'Status Dosen',  'icon' => 'university', 'url' => ['/status-dosen']],
+                            ['label' => 'Universitas',  'icon' => 'book', 'url' => ['/universitas']],
+                            ['label' => 'Wilayah',  'icon' => 'users', 'url' => ['/wilayah']],
+                            ['label' => 'Pendidikan Terakhir',  'icon' => 'graduation-cap', 'url' => ['/pendidikan']],
+                        ],
+                    ],
+                    [
                         'label' => 'Data Master',
                         'icon' => 'database',
                         'items' => [
                             ['label' => 'Fakultas',  'icon' => 'building', 'url' => ['/fakultas']],
-                            ['label' => 'Prodi',  'icon' => 'university', 'url' => ['/gii']],
+                            ['label' => 'Prodi',  'icon' => 'university', 'url' => ['/prodi']],
                             ['label' => 'Mata Kuliah',  'icon' => 'book', 'url' => ['/gii']],
-                            ['label' => 'Dosen',  'icon' => 'users', 'url' => ['/gii']],
-                            ['label' => 'Mahasiswa',  'icon' => 'graduation-cap', 'url' => ['/gii']],
+                            ['label' => 'Dosen',  'icon' => 'users', 'url' => ['/dosen']],
+                            ['label' => 'Mahasiswa',  'icon' => 'graduation-cap', 'url' => ['/mahasiswa']],
                             ['label' => 'Ruang',  'icon' => 'home', 'url' => ['/gii']],
                             ['label' => 'Staff',  'icon' => 'users', 'url' => ['/gii']],
                         ],
