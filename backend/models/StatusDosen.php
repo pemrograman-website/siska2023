de2c7c62a8426da383e1062fa14132ebe8589736
Yusuf Yudhistira <yudhis96@yahoo.com>
2023-05-01 19:44:11 +0700

    Membuat CRUD StatusDosen

---
+++ backend/models/StatusDosen.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "status_dosen".
+ *
+ * @property int $id
+ * @property string $keterangan
+ *
+ * @property Dosen[] $dosens
+ */
+class StatusDosen extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'status_dosen';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['keterangan'], 'required'],
+            [['keterangan'], 'string', 'max' => 45],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'keterangan' => 'Keterangan',
+        ];
+    }
+
+    /**
+     * Gets query for [[Dosens]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getDosens()
+    {
+        return $this->hasMany(Dosen::class, ['status_dosen_id' => 'id']);
+    }
+}
